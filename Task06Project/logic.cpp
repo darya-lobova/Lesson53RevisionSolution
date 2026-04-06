d7177de669318cececc03d3d4f10eb6d8f160e3c
st <st@A510PC3>
2026-04-06 18:20:27 +0300

    Task06 solved

--- Task06Project/logic.cpp
+++ Task06Project/logic.cpp
@@ -3,3 +3,24 @@
 //
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму чётных чисел от 1 до N.
+
+int sum(int number) {
+
+
+	//base case
+	if (number < 0) {
+		return -1;
+	}
+
+	if (number < 2) {
+		return 0;
+	}
+
+
+	if (number % 2 == 1) {
+		number -= 1;
+	}
+	//recursion case
+	return number + sum(number - 2);
+
+}
