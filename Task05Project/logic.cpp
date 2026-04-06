2d924c1c32d8c50a2c7a716379748fafbf783fae
st <st@A510PC3>
2026-04-06 18:00:23 +0300

    Task05 solved

--- Task05Project/logic.cpp
+++ Task05Project/logic.cpp
@@ -3,3 +3,17 @@
 // 
 // Необходимо спроектировать и реализовать программу, 
 // которая подсчитывает количество цифр в заданном целом числе.
+int count(int number) {
+	
+	number = number < 0 ? -number : number;
+
+	//base case
+	if (number < 10) {
+		return 1;
+	}
+
+	//recursion case
+	return 1 + count(number / 10);
+}
+
+
