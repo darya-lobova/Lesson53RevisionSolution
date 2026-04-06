497702402f62f8a6bd228fe13bb67da08ed6b04f
st <st@A510PC3>
2026-04-06 16:49:37 +0300

    Task01 solved

--- Task01Project/logic.cpp
+++ Task01Project/logic.cpp
@@ -3,3 +3,21 @@
 // 
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму чисел от 1 до N.
+
+int sum(int number) {
+
+	//base case
+	if (number < 0) {
+		return -1;
+	}
+	if (number == 0) {
+		return 0;
+	}
+
+	if (number == 1) {
+		return 1;
+	}
+
+	//recursion case
+	return number + sum(number - 1);
+}
