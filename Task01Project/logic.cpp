1062772e53d0077fd7f48d5edfa443d58b25c766
Darya Lobova <dasha.lobova.it@gmail.com>
2026-04-12 06:53:03 +0300

    Update logic.cpp

--- Task01Project/logic.cpp
+++ Task01Project/logic.cpp
@@ -4,19 +4,22 @@
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму чисел от 1 до N.
 
-//Best case: O(1)
-//Worst case: O(N)
-int sum(int number) {
-
+int recursion(int number) {
 	//base case
-	if (number < 0) {
-		return -1;
-	}
-
-	if (number == 0) {
-		return 0;
+	if (number == 1) {
+		return 1;
 	}
 
 	//recursion case
-	return number + sum(number - 1);
+	return number + recursion(number - 1);
+}
+
+#include "logic.h"
+
+int sum(int number) {
+	
+	if (number <= 0) {
+		return -1;
+	}
+	return recursion(number);
 }
