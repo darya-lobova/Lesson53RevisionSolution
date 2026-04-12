f300fbe03c651c3fafedbfa05028b99d1a074728
Darya Lobova <dasha.lobova.it@gmail.com>
2026-04-12 06:56:46 +0300

    Update logic.cpp

--- Task12Project/logic.cpp
+++ Task12Project/logic.cpp
@@ -5,3 +5,22 @@
 // для вычисления n-го числа в последовательности, 
 // где каждое следующее число равно сумме предыдущего 
 // и его номера: 1, 2, 4, 7, 11, 16, ...
+#include "logic.h"
+
+int recursion(int index) {
+	//base case
+	if (index == 1) {
+		return 1;
+	}
+	//recursion case
+	index--;
+	return recursion(index) + index;
+}
+
+int get_number(int index) {
+
+	if (index <= 0) {
+		return -1;
+	}
+	return recursion(index);
+}
