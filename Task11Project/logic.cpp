ca56fc1dd921df7ca68959c777d3919453bc5927
Darya Lobova <dasha.lobova.it@gmail.com>
2026-04-12 06:55:51 +0300

    Update logic.cpp

--- Task11Project/logic.cpp
+++ Task11Project/logic.cpp
@@ -3,3 +3,24 @@
 //
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет среднее арифметическое элементов вектора.
+#include "logic.h"
+
+double recursion(int vector[], int size) {
+	//base case
+	if (size == 1) {
+		return vector[0] > 0 ? 1 : 0;
+	}
+	//recursion case
+	size--;
+	return recursion(vector, size) + vector[size];
+}
+
+double calculate_average(int vector[], int size) {
+
+	//passive fool-proof
+	if (size <= 0) {
+		return -1;
+	}
+
+	return recursion(vector, size) / size;
+}
