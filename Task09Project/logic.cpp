830649e3a046901cada009a01025724381736891
Darya Lobova <dasha.lobova.it@gmail.com>
2026-04-12 06:54:55 +0300

    Update logic.cpp

--- Task09Project/logic.cpp
+++ Task09Project/logic.cpp
@@ -3,3 +3,20 @@
 //
 // Необходимо спроектировать и реализовать программу, 
 // которая вычисляет сумму цифр заданного числа, кратных 5
+#include "logic.h"
+
+int recursion(int number) {
+	//base case
+	if (number < 10) {
+		return number == 5 ? 5 : 0;
+	}
+
+	//recursion case
+	return recursion(number / 10) + (number % 10 == 5 ? 5 : 0);
+}
+
+int sum(int number) {
+	number *= number < 0 ? -1 : 1;
+
+	return recursion(number);
+}
